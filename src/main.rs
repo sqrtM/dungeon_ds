0e65b0631b912e42c5ce4ba2b462b2aa95185519
mason :) <masonapike@gmail.com>
2023-12-24 22:07:26 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use ctru::prelude::*;
+
+fn main() {
+    let apt = Apt::new().unwrap();
+    let mut hid = Hid::new().unwrap();
+    let gfx = Gfx::new().unwrap();
+    let _console = Console::new(gfx.top_screen.borrow_mut());
+
+    println!("Hello, World!");
+    println!("\x1b[29;16HPress Start to exit");
+
+    while apt.main_loop() {
+        gfx.wait_for_vblank();
+
+        hid.scan_input();
+        if hid.keys_down().contains(KeyPad::START) {
+            break;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_add_numbers() {
+        assert_eq!(2, 2);
+    }
+}
